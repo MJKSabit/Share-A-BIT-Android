c780e1a93ede735cd45b9317fbc97f7cce25689a
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-07-13 12:04:16 +0600

    Info Layout Implementation

--- app/src/main/java/github/mjksabit/sabit/android/ui/InformationFragment.java
+++ app/src/main/java/github/mjksabit/sabit/android/ui/InformationFragment.java
@@ -1,9 +1,12 @@
 package github.mjksabit.sabit.android.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,9 +15,20 @@ import github.mjksabit.sabit.android.R;
 
 public class InformationFragment extends Fragment {
 
+    private Button update;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_info, container, false);
+        update = root.findViewById(R.id.update_check);
+        update.setOnClickListener((v) -> checkUpdate());
+
         return root;
+    }
+
+    private void checkUpdate() {
+        String link = getResources().getString(R.string.update_link);
+        Intent webBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+        startActivity(webBrowser);
     }
 }
